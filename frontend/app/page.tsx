57788ee30f16cd305b7ead2c7bf50e46cc28c3e5
Amit Bera <amitalokbera@gmail.com>
2025-07-07 01:03:05 +0530

    feat: initialize frontend with Next.js, TypeScript, and Tailwind CSS

---
+++ frontend/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div className="flex w-full h-full m-0 p-0">
+      <div className="bg-red-300 flex-1">Login Component</div>
+      <div className="bg-blue-300 flex-1">Auth Hero Section</div>
+    </div>
+  );
+}
